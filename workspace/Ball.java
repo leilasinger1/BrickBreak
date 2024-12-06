76d8f4a3404038334a541356380857ed076b2ed2
leilasinger1 <girlswhocodewj@gmail.com>
2024-12-06 19:27:14 +0000

    submission 12/6

--- workspace/Ball.java
+++ workspace/Ball.java
@@ -1,13 +1,84 @@
 import java.awt.*;
-
+//Leila Singer
+//12/6/24
+//Initiates ball for the overall code in order to make it properly work
 public class Ball {
 	//your code here!
-	
 	//don't forget you need instance variables:
 	
+private int ypos;
+private int xpos;
+private int size;
+private int Xvelocity;
+private int Yvelocity;
+
 	
 	//constructor(s):
-	
-	//methods:
+	public Ball(int x, int y, int size) {
+		this.xpos=x;
+		this.ypos=y;
+		this.size = size;
+		Xvelocity = 1;
+		Yvelocity = 1;
+	}
+	//getters and setters
+	 public  int getYpos(){
+		return ypos;
+	 }
+	public int getXpos(){
+		return xpos;
+	}
+	public int getSize(){
+		return size;
+	}
+	public void setY (int ypos){
+		this.ypos = ypos;
+	}
+	public void setX (int xpos){
+		this.xpos = xpos;
+	}
+	public void setSize (int size){
+		this.size = size;
+	}
+	public void setXVelocity(int Xvelocity){
+		this.Xvelocity=Xvelocity;
+	}
+	public void setYVelocity(int YVelocity){
+		this.Yvelocity = YVelocity;
+	}
+	//precondition: Xvelocity is set correctly
+	//postcondition: reverses x value when it hits a block
+	public void reverseX(){
+		Xvelocity *=-1;
+	}
+	//precondition: Yvelocity is set correctly
+	//postcondition: reverses y value when it hits a block
+	public void reverseY(){
+		Yvelocity*=-1;
+	}
+//precondition: variable g is properly declared
+//postcondition: Ball is set to the color white, and made into an oval
+public void draw (Graphics g){
+  g.setColor(Color.white);
+  g.fillOval(xpos, ypos, size, size);
+}
+//precondition: xpos, ypos, xvelocity, and yvelocity all work correctly
+//postcondition: Moves the ball
+public void move (){
+	xpos+=Xvelocity;
+	ypos+=Yvelocity;
+}
+
+// .draw
+// .move
+
+
+
+
+
 
 }
+
+
+
+
--- workspace/Paddle.java
+++ workspace/Paddle.java
@@ -1,16 +1,88 @@
-import java.awt.Graphics;
 import java.awt.*;
-
+//Leila Singer
+//12/6/24
+//Initates paddle variable in order to make it correctly work overall.
 public class Paddle {
 
 	//your code here!
 	
 	//don't forget you need instance variables:
-	
-	
+	private int x;
+private int y;
+private int height;
+private int width;
+private int Xvelocity;
 	//constructor(s):
-	
+public Paddle (int x,int y, int width, int height){
+	this.x = x;
+	this.y = y;
+	this.width = width;
+	this.height = height;
+}
 	//methods:
+
+	public  int getY(){
+		return y;}
+
+	public int getX(){
+		return x;
+
+	}
+
+	public int getHeight(){
+		return height;
+
+	}
+
+	public int getWidth(){
+		return width;
+	}
 	
+	public void setWidth(int width){
+		this.width=width;
+	}
+	public void setHeight(int height){
+		this.height=height;
+
+	}
+
+
+	public void setX(int x){
+		this.x=x;
+	}
+
+	public void setY(int y){
+		this.y=y;
+	}
+	
+	
+	
+	public int getVelocity(){
+		return Xvelocity;
+	}
+	public void setVelocity(int Xvelocity){
+this.Xvelocity=Xvelocity;
+	}
+	
+	//precondition: variable g must be properly declared
+	//postcondition: sets paddle color to white, sets the shape to oval
+	public void draw (Graphics g){
+  g.setColor(Color.white);
+  g.fillOval(x, y, width, height);
+}
+//precondition: velocity and Xvelocity are set correctly
+//postcondition: adds velocity to x value
+public void addVelocity (int velocity){
+	Xvelocity = velocity;
+}
+//precondition: x value and xvelocity work correctly
+//postcondition: moves x value by x velocity
+public void move(){
+	x+=Xvelocity;
+
+}
+		
+
+
 	
 }
